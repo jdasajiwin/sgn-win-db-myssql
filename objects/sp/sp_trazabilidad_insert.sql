b029bf0fdab8b88ab06354b1765ebcdb8fe93189
Fuyu Collantes <fcollantes.soaint@win.pe>
2026-05-14 17:55:38 -0500

    trazabilidad add

---
+++ objects/sp/sp_trazabilidad_insert.sql
@@ -0,0 +1,36 @@
+DROP PROCEDURE IF EXISTS sp_trazabilidad_insert;
+
+DELIMITER $$
+CREATE PROCEDURE sp_trazabilidad_insert(
+    IN p_id_usuario INT,
+    IN p_id_menu INT,
+    IN p_evento VARCHAR(100),
+    IN p_detalle TEXT,
+    IN p_datos_anteriores JSON,
+    IN p_datos_nuevos JSON,
+    IN p_ip_origen VARCHAR(45),
+    IN p_creado_por_api TINYINT(1)
+)
+BEGIN
+    INSERT INTO t_trazabilidad_eventos (
+        id_usuario,
+        id_menu,
+        evento,
+        detalle,
+        datos_anteriores,
+        datos_nuevos,
+        ip_origen,
+        creado_por_api
+    ) VALUES (
+        p_id_usuario,
+        p_id_menu,
+        p_evento,
+        p_detalle,
+        p_datos_anteriores,
+        p_datos_nuevos,
+        p_ip_origen,
+        p_creado_por_api
+    );
+
+END$$
+DELIMITER ;
