4dd7273487b8d3c6dc953d0be6bc504f90e374e2
Raúl Romero <rromero.soaint@win.pe>
2026-06-03 11:57:30 -0500

    feat: Querys para listar usuarios con/sin filtros

--- query/query_users.sql
+++ query/query_users.sql
@@ -8,6 +8,13 @@ SELECT * FROM m_roles;
 SELECT * FROM t_usuario_rol;
 SELECT * FROM m_roles_menus;
 
-CALL spl_t_usuarios_principal_listar ('jsdaniel_21@hotmail.com')
-CALL spl_m_roles_listar('1')
-CALL spl_m_roles_menus_listar(2)
+CALL spl_t_usuarios_principal_listar ('jsdaniel_21@hotmail.com');
+
+-- Listado con filtros opcionales (NULL = sin filtro)
+CALL spl_t_usuarios_listar(NULL, NULL, NULL);
+CALL spl_t_usuarios_listar('fuyu', NULL, NULL);
+CALL spl_t_usuarios_listar(NULL, 1, NULL);
+CALL spl_t_usuarios_listar(NULL, NULL, 1);
+CALL spl_t_usuarios_listar('romero', 1, 1);
+CALL spl_m_roles_listar('1');
+CALL spl_m_roles_menus_listar(2);
