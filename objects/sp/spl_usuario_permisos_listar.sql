74d43407ece8aeea71340e7c0ef245d55c329df4
Raúl Romero <rromero.soaint@win.pe>
2026-06-12 12:56:42 -0500

    fix: Corregir DELIMITIR en SP spl_usuario_permisos_listar

--- objects/sp/spl_usuario_permisos_listar.sql
+++ objects/sp/spl_usuario_permisos_listar.sql
@@ -1,8 +1,8 @@
 -- Stored Procedure para listar los permisos de un usuario
 -- Obtiene todos los permisos asociados a los roles del usuario
-DELIMITER //
+DELIMITER $$
 
-DROP PROCEDURE IF EXISTS spl_usuario_permisos_listar //
+DROP PROCEDURE IF EXISTS spl_usuario_permisos_listar $$
 
 CREATE PROCEDURE spl_usuario_permisos_listar(
     IN p_id_rol INT
@@ -11,13 +11,13 @@ BEGIN
     SELECT DISTINCT
         p.cod_permiso,
         p.desc_accion,
-        p.desc_permiso,
+        p.desc_permiso
     FROM m_roles_permisos rp
     INNER JOIN m_permisos p ON p.id_permiso = rp.id_permiso
     WHERE rp.id_rol = p_id_rol
         AND rp.flg_activo = 1
         AND p.flg_activo = 1
     ORDER BY p.desc_modulo, p.desc_submodulo, p.desc_accion;
-END //
+END $$
 
 DELIMITER ;
